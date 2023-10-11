53edb03eb5f591d65b926fdec063a73e7558c82d
Guillaume Le Saux <guillaume.le_saux@edu.devinci.fr>
2023-10-11 11:38:06 +0200

    final push

--- main.js
+++ main.js
@@ -24,17 +24,27 @@ class Pokemon {
     let resthp = adversaire.hp - this.attak;
     console.log(this.name + " n'a plus que " + resthp + " point de vie");
   }
+  isdead(adversaire) {
+    let mort = (poke1.hp = 0);
+    console.log(this.name + " est mort");
+  }
 }
 
-let poke1 = new Pokemon("Pikachu", 5, 4, 1, 0.1);
-let poke2 = new Pokemon("Ronflex", 3, 5, 4, 0.7);
+let poke1 = new Pokemon("Pikachu", 5, 4, 20, 0.8);
+let poke2 = new Pokemon("Ronflex", 3, 5, 12, 0.7);
 
 while (poke1.hp > 0 && poke2.hp > 0) {
-  console.log("salut");
+  console.log("Attaque enfoire");
   if (poke1.isLucky()) {
     poke1.attakPokemon(poke2);
   } else {
-    console.log("passe ton tour frerot");
+    poke1.isdead(poke2);
+    break;
+  }
+  if (poke2.isLucky()) {
+    poke2.attakPokemon(poke1);
+  } else {
+    poke1.isdead(poke1);
     break;
   }
 }
