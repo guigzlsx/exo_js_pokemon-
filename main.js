c213e484fec3a230b365235103f88d3a3d6275b1
Guillaume Le Saux <guillaume.le_saux@edu.devinci.fr>
2023-10-11 09:00:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+class Pokemon {
+  constructor(name, attak, defense, hp, luck) {
+    this.name = name;
+
+    this.attak = attak;
+
+    this.defense = defense;
+
+    this.hp = hp;
+
+    this.luck = luck;
+  }
+  getphpLvl() {
+    console.log(this.name + " a " + this.php + "/5 en php");
+  }
+}
+
+let pikachu = new Pokemon("Pikachu", 5, 4, 1, 3);
+let ronflex = new Pokemon("Ronflex", 3, 5, 4, 2);
+
+
+while(this.hp != 0){
+    
+}
